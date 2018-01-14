2b4600ef2dfcc74737b7db59533029f8aee62e27
GregSied <fora74@o2.pl>
2018-01-14 12:05:27 +0100

    click + display

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import {Observable} from "rxjs";
-
 //by element id moze
-
 const button = document.querySelector('button');
 const output = document.querySelector('#output');
 
@@ -27,13 +25,25 @@ function load(url: string) {
         xhr.open("GET", url);
         xhr.send();
     })
+        .map((a) => a.sort((a,b) => a.pages > b.pages))
 }
 
 // load("/books-api.json");
 
+function renderBooks(books){
+    books.forEach(b => {
+        let node = document.createElement('div');
+        node.innerText="title: "+b.title+", pages:"+b.pages;
+        output.appendChild(node);
+    })
+}
+
 click.flatMap(e => load("/books-api.json"))
     .subscribe(
-        (e) => console.log(e),
-        (e) => console.log(`error: ${e}`),
-            () => console.log('done')
+        (e) => {
+            console.log(e);
+            renderBooks(e);
+        },
+                (e) => console.log(`error: ${e}`),
+                () => console.log('done')
     );
